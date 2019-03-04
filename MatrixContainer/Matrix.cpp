64db7369b8de0852cc4e0546054694b732469f0e
damadam <damadam@10.0.0.54>
2019-03-03 22:22:38 -0600

    Adding Matrix class.

---
+++ MatrixContainer/Matrix.cpp
@@ -0,0 +1,18 @@
+/*
+ * Matrix.cpp
+ *
+ *  Created on: Mar 3, 2019
+ *      Author: damadam
+ */
+
+#include "Matrix.h"
+
+Matrix::Matrix() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Matrix::~Matrix() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ MatrixContainer/Matrix.h
@@ -0,0 +1,17 @@
+/*
+ * Matrix.h
+ *
+ *  Created on: Mar 3, 2019
+ *      Author: damadam
+ */
+
+#ifndef MATRIX_H_
+#define MATRIX_H_
+
+class Matrix {
+public:
+	Matrix();
+	virtual ~Matrix();
+};
+
+#endif /* MATRIX_H_ */
